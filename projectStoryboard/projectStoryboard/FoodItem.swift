b92fcecae20531df788f2077b9d9137fcc8e3065
seanq101 <seanq101@gmail.com>
2019-05-21 16:19:15 -0700

    yeet yaw

---
+++ projectStoryboard/projectStoryboard/FoodItem.swift
@@ -0,0 +1,34 @@
+//
+//  FoodItem.swift
+//  projectStoryboard
+//
+//  Created by Sean Quinn on 5/21/19.
+//  Copyright © 2019 Sean Quinn. All rights reserved.
+//
+
+import Foundation
+
+struct FoodItem : Codable {
+    
+    
+    let properName: String
+    let apiName: String
+    let quantity: Int
+    let unit: String
+    let expiry: Date
+    
+//    enum UnitEnum {
+//        case oz
+//        case lb
+//        case kg
+//        case cup
+//        case liter
+//        case gallon
+//        case tbsp
+//        case tsp
+//        case other
+//    }
+    
+    
+    
+}
