b013ebb7c6c85ccf5e75811a97602ddf7f491dd0
seanq101 <seanq101@gmail.com>
2019-05-21 15:11:06 -0700

    Initial stuff from milestone 2

---
+++ projectStoryboard/projectStoryboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projectStoryboard
+//
+//  Created by Sean Quinn on 4/23/19.
+//  Copyright © 2019 Sean Quinn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
